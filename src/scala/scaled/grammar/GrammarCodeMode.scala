c4bcbda6a881255bb0ef32439b10f46466f246df
Michael Bayne <mdb@samskivert.com>
2019-02-21 07:19:35 -0800

    Factor out and document the style "clearer" predicates.

--- src/scala/scaled/grammar/GrammarCodeMode.scala
+++ src/scala/scaled/grammar/GrammarCodeMode.scala
@@ -41,12 +41,25 @@ abstract class GrammarCodeMode (env :Env) extends CodeMode(env) {
     frame.revisitFile()
   }
 
+  /** When a line changes and the TextMate grammars are reapplied to compute new syntax
+    * highlighting, we must remove all of the old styles prior to restyling the line. However, not
+    * all styles applied to the line come from syntax highlighting. This function must indicate
+    * whether the supplied style was a result of syntax highlighting or not.
+    *
+    * The approach used by `GrammarCodeMode` is to prefix all syntax highlighting styles with
+    * `code` to distinguish them from non-syntax highlighting styles. If you add additional styles,
+    * you should either follow this `code` prefix (i.e. `codeFunction`) or override this method to
+    * additionally return true for your styles (by perhaps keeping all your styles in a set and
+    * checking the style for inclusion in that set).
+    */
+  protected def isModeStyle (style :String) = style startsWith "code"
+
   private def mkProcs (plugin :GrammarPlugin) = {
     val procs = List.builder[Selector.Processor]()
     if (!plugin.effacers.isEmpty) procs += new Selector.Processor(plugin.effacers) {
       override def onBeforeLine (buf :Buffer, row :Int) { // clear any code styles
         val start = buf.lineStart(row) ; val end = buf.lineEnd(row)
-        if (start != end) buf.removeTags(classOf[String], (_ :String) startsWith "code", start, end)
+        if (start != end) buf.removeTags(classOf[String], isModeStyle, start, end)
       }
     }
     if (!plugin.syntaxers.isEmpty) procs += new Selector.Processor(plugin.syntaxers) {
--- src/scala/scaled/grammar/GrammarTextMode.scala
+++ src/scala/scaled/grammar/GrammarTextMode.scala
@@ -35,11 +35,24 @@ abstract class GrammarTextMode (env :Env) extends TextMode(env) {
   @Fn("Refreshes the colorization of the entire buffer.")
   def refaceBuffer () :Unit = scoper.rethinkBuffer()
 
+  /** When a line changes and the TextMate grammars are reapplied to compute new syntax
+    * highlighting, we must remove all of the old styles prior to restyling the line. However, not
+    * all styles applied to the line come from syntax highlighting. This function must indicate
+    * whether the supplied style was a result of syntax highlighting or not.
+    *
+    * The approach used by `GrammarTextMode` is to prefix all syntax highlighting styles with
+    * `text` to distinguish them from non-syntax highlighting styles. If you add additional
+    * styles, you should either follow this `text` prefix (i.e. `textParagraphHeader`) or override
+    * this method to additionally return true for your styles (by perhaps keeping all your styles
+    * in a set and checking the style for inclusion in that set).
+    */
+  protected def isModeStyle (style :String) = style startsWith "text"
+
   private def mkProcs (plugin :GrammarPlugin) =
     if (plugin.effacers.isEmpty) Nil else List(new Selector.Processor(plugin.effacers) {
       override def onBeforeLine (buf :Buffer, row :Int) { // clear any text styles
         val start = buf.lineStart(row) ; val end = buf.lineEnd(row)
-        if (start != end) buf.removeTags(classOf[String], (_ :String) startsWith "text", start, end)
+        if (start != end) buf.removeTags(classOf[String], isModeStyle, start, end)
       }
     })
 }
